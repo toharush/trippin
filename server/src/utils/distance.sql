6f2e9470e54a07eb2b22d86f3cdb1062f5756aa6
Toharush <toahr1010@gmail.com>
2022-12-16 19:33:19 +0200

    Added sql query for nearby places

---
+++ server/src/utils/distance.sql
@@ -0,0 +1,22 @@
+SELECT * from (SELECT id, "position", ( 6371 * 
+    ACOS( 
+        COS( RADIANS( "position"[0] ) ) * 
+        COS( RADIANS( -71.060316 ) ) * 
+        COS( RADIANS( 50 ) - 
+        RADIANS( "position"[1]  ) ) + 
+        SIN( RADIANS( "position"[0] ) ) * 
+        SIN( RADIANS( -71.060316) ) 
+    ) 
+) "distance" FROM trippin.place) "place" WHERE "place"."distance" <= 100 ORDER BY "distance" ASC;
+
+SELECT * from (SELECT id, "position",
+( 6371 * 
+    ACOS( 
+        COS( RADIANS( db_latitude ) ) * 
+        COS( RADIANS( $user_latitude ) ) * 
+        COS( RADIANS( $user_longitude ) - 
+        RADIANS( db_longitude ) ) + 
+        SIN( RADIANS( db_latitude ) ) * 
+        SIN( RADIANS( $user_latitude) ) 
+    ) 
+) "distance" FROM original_Table) the_table WHERE the_table."distance" <= he_radius ORDER BY "distance" ASC;
