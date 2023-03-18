b69bc8f91331bcdb2bf1c0448b0c49a80a11a0ee
ofriaker <ofriaker@gmail.com>
2023-03-18 17:57:52 +0200

    different dropdown

--- client/src/components/TravelsCategoryComponent/TravelsCategoryComponent.tsx
+++ client/src/components/TravelsCategoryComponent/TravelsCategoryComponent.tsx
@@ -4,32 +4,22 @@ import { useStyles } from './TravelsCategoryComponentStyle';
 import Dropdown from '../Dropdown/Dropdown';
 import CategoriesGallery from '../CategoriesGallery/CategoriesGallery';
 import { useState } from 'react';
-import ActivitiesGallery from '../ActivitiesGallery/ActivitiesGallery';
-
 
 export default function TravelsCategoryComponent() {
 
 const [isCategoriesOpen, setIsCategoriesOpen] = useState(false);
-const [isActivitiesOpen, setIsActivitiesOpen] = useState(false);
 
 const handleCategoriesClick = () => {
   setIsCategoriesOpen(!isCategoriesOpen);
 }   
 
-const handleActivitiesClick = () => {
-  setIsActivitiesOpen(!isActivitiesOpen);
-}
-
 const classes = useStyles();
 const categoriesTitle="Travel's Categories";
-const activitiesTitle="Selected Activities";
 
   return (
     <Box>
       <Dropdown handleClick={handleCategoriesClick} isCategoryOpen={isCategoriesOpen} title={categoriesTitle}></Dropdown>
       {isCategoriesOpen && <CategoriesGallery></CategoriesGallery> }
-      <Dropdown handleClick={handleActivitiesClick} isCategoryOpen={isActivitiesOpen} title={activitiesTitle}></Dropdown>
-        {isActivitiesOpen && <ActivitiesGallery></ActivitiesGallery>}
     </Box>
   );
 }
