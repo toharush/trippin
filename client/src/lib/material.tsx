dd8370efdca0ba34a029ac8009421a71613d3977
Mr Harush <toharush@toharush.home>
2023-03-31 10:21:14 +0300

    some fixes

--- client/src/lib/material.tsx
+++
@@ -1,5 +0,0 @@
-import { makeStyles as makeStyle } from "@mui/styles";
-
-export const makeStyles = (stylesCreator: any, options?: object) => {
-  return makeStyle(stylesCreator, options) as Function;
-};
