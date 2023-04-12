380a0ccbcf65e553e5ebb802be300e18c1d3a324
Mr Harush <toharush@toharush.home>
2023-04-12 12:08:18 +0300

    prettier

--- apps/replicator/src/utils/database/client.ts
+++ apps/replicator/src/utils/database/client.ts
@@ -1,18 +1,18 @@
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
 dotenv.config();
 export const enum QueryResult {
   FIELD_NOT_FOUND = -2,
-  COLUMN_NOT_FOUND = -1
+  COLUMN_NOT_FOUND = -1,
 }
 
 const client = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "postgres",
   port: Number(process.env.DB_PORT) || 5324,
-  max: 1
+  max: 1,
 });
 
 export default client;
--- apps/replicator/src/utils/database/init-db.ts
+++ apps/replicator/src/utils/database/init-db.ts
@@ -2,33 +2,30 @@ import logger from "../logger/logger";
 import client from "./client";
 import { schema } from "./config";
 import { create_address } from "./init/address";
-import { create_category } from './init/category';
+import { create_category } from "./init/category";
 import { create_extra_categories } from "./init/extra_categoris";
 import { create_google } from "./init/google";
-import { create_place } from './init/place';
+import { create_place } from "./init/place";
 import { create_position } from "./init/position";
 
 const create_schema = `CREATE SCHEMA IF NOT EXISTS ${schema}`;
 const drop_schema = `DROP SCHEMA ${schema} CASCADE;`;
 
-export const init_database = async() => {
-    try {
-        await client.query(create_schema);
-        await client.query(create_category);
-        await client.query(create_position);
-        await client.query(create_address);
-        await client.query(create_place);
-        await client.query(create_extra_categories);
-        await client.query(create_google);
-    } catch (err) {
-        logger.error("failed to init database");
-        logger.error(err);
-    }
-}
+export const init_database = async () => {
+  try {
+    await client.query(create_schema);
+    await client.query(create_category);
+    await client.query(create_position);
+    await client.query(create_address);
+    await client.query(create_place);
+    await client.query(create_extra_categories);
+    await client.query(create_google);
+  } catch (err) {
+    logger.error("failed to init database");
+    logger.error(err);
+  }
+};
 
-export const drop_database = async() => {
-    await client.query(drop_schema);
-}
-
-
-
+export const drop_database = async () => {
+  await client.query(drop_schema);
+};
