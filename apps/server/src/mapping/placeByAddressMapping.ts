a9523c3c81929a29c9220c9ada5c6f6393859527
YaronZandberg <52877527+YaronZandberg@users.noreply.github.com>
2023-04-29 16:31:10 +0300

    Fix Tohar comments

--- apps/server/src/mapping/placeByAddressMapping.ts
+++ apps/server/src/mapping/placeByAddressMapping.ts
@@ -1,23 +1,16 @@
-import { schema as DBSchema, TABLES } from '../../../../utils';
+import { schema, TABLES } from '../../../../utils';
 
 function getPlaceSQLQuery(countryCode: string, stateName?: string, cityName?: string): string {
-    var sqlQuery;
+    let sqlQuery = `(SELECT id FROM ${schema}.${TABLES.ADDRESS} WHERE country_code='${countryCode}'`;
 
-    if(stateName == undefined) {
-        if (cityName == undefined) {
-            sqlQuery = `(SELECT id FROM ${DBSchema}.${TABLES.ADDRESS} WHERE country_code='${countryCode}')`;
-        } else {
-            sqlQuery = `(SELECT id FROM ${DBSchema}.${TABLES.ADDRESS} WHERE country_code='${countryCode}' AND city='${cityName}')`;
-        }
-    } else {
-        if (cityName == undefined) {
-            sqlQuery = `(SELECT id FROM ${DBSchema}.${TABLES.ADDRESS} WHERE country_code='${countryCode}' AND state='${stateName}')`;
-        } else {
-            sqlQuery = `(SELECT id FROM ${DBSchema}.${TABLES.ADDRESS} WHERE country_code='${countryCode}' AND state='${stateName}' AND city='${cityName}')`;
-        }
+    if (stateName !== undefined) {
+        sqlQuery += ` AND state='${stateName}'`;
+    }
+    if (cityName !== undefined) {
+        sqlQuery += ` AND city='${cityName}'`;
     }
 
-    return sqlQuery;
+    return sqlQuery + ')';
 }
 
 export default getPlaceSQLQuery;
