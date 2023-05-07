5c2f5924dab6bd6a64e8db8b583295845d48dee0
Mr Harush <toharush@toharush.home>
2023-05-07 09:51:51 +0300

    change to if

--- apps/client/src/hooks/useMapDrawer.tsx
+++ apps/client/src/hooks/useMapDrawer.tsx
@@ -6,22 +6,13 @@ import { AddMarkerPoint, RemoveMarkerPoint } from "../store/slices/map";
 
 const useMapDrawer = () => {
   const dispatch = useAppDispatch();
-
   const markers = useSelector(selectMarkerPoints);
-  const addMarker = async (markerPoint: MarkerPoint) => {
-    await dispatch(AddMarkerPoint(markerPoint));
-  };
-  const removeMarker = async (id: string) => {
-    await dispatch(RemoveMarkerPoint(id));
-  };
 
-  const addMarkerPoint = (obj: MarkerPoint) => {
-    // @ts-ignore
-    addMarker(obj);
-  };
-  const removeMarkerPoint = (id: string) => {
-    removeMarker(id);
-  };
+  const addMarkerPoint = async (markerPoint: MarkerPoint) =>
+    await dispatch(AddMarkerPoint(markerPoint));
+  const removeMarkerPoint = async (id: string) =>
+    await dispatch(RemoveMarkerPoint(id));
+
   return {
     markers,
     addMarkerPoint,
