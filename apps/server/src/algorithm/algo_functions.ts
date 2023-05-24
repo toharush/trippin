f53f73e130b903cc34d314ec150373f6cf4616d8
YaronZandberg <52877527+YaronZandberg@users.noreply.github.com>
2023-05-24 17:14:50 +0300

    1. Fix filter activities method. 2. Create rank activities method.

--- apps/server/src/algorithm/algo_functions.ts
+++ apps/server/src/algorithm/algo_functions.ts
@@ -1,19 +1,43 @@
 import {Activity} from "../../../client/src/interfaces";
 
 export const filterCoveredActivities = (allActivities: Activity[], coveredActivities: Activity[]) => {
-    let potentialActivities = allActivities.slice();
-
-    for (let index = 0; index < potentialActivities.length; index++) {
-        if (potentialActivities[index] == coveredActivities[index]) {
-            potentialActivities = potentialActivities.slice(index, index);
-        }
-    }
+    let potentialActivities: Activity[] = [];
+    
+    allActivities.forEach(currentActivity => {
+        if (!isActivityCovered(currentActivity, coveredActivities)) {
+            potentialActivities.push(currentActivity);
+        } 
+    });
 
     return potentialActivities;
 }
 
-const gradeActivities = (categoryPriorities: Map<number, number>, activitiesInRadius: Activity[]) => {
-    let rankedActivities = Map<Activity, number>;
+const isActivityCovered = (activity: Activity, activities: Activity[]) => {
+    if (activities.includes(activity)) {
+        return true;
+    }
+    return false;
+}
 
 
+export const getRankedActivities = (categoryPriorities: Map<String, number>, potentialActivities: Activity[]) => {
+    let rankedActivities: Map<Activity, number> = new Map();
+
+    potentialActivities.forEach(currentActivity => {
+        let grade = getActivityGrade(currentActivity, categoryPriorities);
+        rankedActivities.set(currentActivity, grade);
+    })
+
+    return rankedActivities;
+}
+
+const getActivityGrade = (activity: Activity, categoryPriorities: Map<String, number>) => {
+    // Change 5 to activity.rating after inserting rating for every activity in the DB
+    let activityRating: number = 5;
+    let categoryPreference: number | undefined = categoryPriorities.get(activity.category);
+    
+    if (categoryPreference !== undefined) {
+        return activityRating * categoryPreference;
+    }
+    return 0;    
 }
