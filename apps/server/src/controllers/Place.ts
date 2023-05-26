dd946fafc62f92573167e78d49a9bade33fc5be1
YaronZandberg <52877527+YaronZandberg@users.noreply.github.com>
2023-05-26 14:16:19 +0300

    Create a function that get all places from DB in specific radius
    1. Create the SQL query, put it in distance.sql and check it in pgAdmin.
    2. Export a connection to DB in sql_client.
    3. Updating app.ts to use the sql_client we have just created.
    4. Create a general method that uses a SQL query and an array of parameters.
    5. Create a place model which contacts the DB.
    6. Create a place controller which call the place model method.
    7. Create a place interface.

--- apps/server/src/controllers/Place.ts
+++ apps/server/src/controllers/Place.ts
@@ -1,2 +1,10 @@
-import Place from "../models/place/place";
-import * as joinMonster from 'join-monster';
+import { getPlacesInRadiusDB } from '../models/native/place';
+import ICoordinate from '../../../client/src/interfaces/activity/coordinate';
+import Place from '../interfaces/place';
+
+export const getPlacesInRadius = async (
+    radius: number,
+    centerPoint: ICoordinate
+): Promise<Place[]> => {
+    return await getPlacesInRadiusDB(radius, centerPoint);
+};
