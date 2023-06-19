867d526c6c0031a45cdaa646a10df8ecba0ee485
amitsabag7 <60693533+amitsabag7@users.noreply.github.com>
2023-06-19 23:08:53 +0300

    Added travel time in client

--- apps/client/src/components/DayRouteComponent/DayRouteComponent.tsx
+++ apps/client/src/components/DayRouteComponent/DayRouteComponent.tsx
@@ -3,7 +3,7 @@ import IDailyRoute from "../../interfaces/activity/dailyRoute";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import OneStopInDayRouteComponent from "../OneStopInDayRouteComponent/OneStopInDayRouteComponent";
 import { ITripActivity } from "../../interfaces";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 interface props {
   dayRoute: IDailyRoute;
@@ -11,12 +11,35 @@ interface props {
 
 const DayRouteComponent = ({ dayRoute }: props) => {
   const paperRef = useRef<HTMLDivElement>(null);
+  const [travelTimesArray, setTravelTimesArray] = React.useState<string[]>([]);
 
   React.useEffect(() => {
     if (paperRef.current) {
       paperRef.current.scrollTop = 0;
     }
+    setTravelTimesArray(createTravelActivitiesArray());
   }, [dayRoute]);
+
+  const createTravelActivitiesArray = () => {
+    const travelTimes: string[] =[];
+    for (let i = 1; i < dayRoute.activities.length; i++) {
+      const currentActivity = dayRoute.activities[i] as ITripActivity;
+      const previousActivity  = dayRoute.activities[i-1] as ITripActivity;
+      const travelTime = new Date(currentActivity.start_time).getTime() - new Date(previousActivity.end_time).getTime();
+      const travelTimeFormated =  millisecondsToTimeString(travelTime);
+      travelTimes.push(travelTimeFormated);
+    }
+    return travelTimes;
+  }
+
+  const millisecondsToTimeString = (milliseconds: number): string => {
+    const totalSeconds = Math.floor(milliseconds / 1000);
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+  
+    return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
 
   return (
     <Paper
@@ -32,11 +55,12 @@ const DayRouteComponent = ({ dayRoute }: props) => {
         marginBottom:'3%'
       }}
     >
-      <LocationOnIcon sx={{ color: "#86eaf0" }}></LocationOnIcon>
-      {dayRoute.activities.map((activity) => (
+      {dayRoute.activities.map((activity,index) => (
         <OneStopInDayRouteComponent
           key={(activity as ITripActivity).activity.id}
           activity={activity as ITripActivity}
+          travelTime={travelTimesArray[index]}
+          lastStop={index===dayRoute.activities.length-1}
         ></OneStopInDayRouteComponent>
       ))}
     </Paper>
