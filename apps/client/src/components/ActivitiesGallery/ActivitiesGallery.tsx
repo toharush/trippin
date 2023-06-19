fbd1ef9d484739a40377d7c2a6fcd78548099705
amitsabag7 <60693533+amitsabag7@users.noreply.github.com>
2023-06-20 00:22:22 +0300

    fix scroll bug

--- apps/client/src/components/ActivitiesGallery/ActivitiesGallery.tsx
+++ apps/client/src/components/ActivitiesGallery/ActivitiesGallery.tsx
@@ -2,18 +2,21 @@ import { Stack } from "@mui/system";
 import { Activity as activity } from "../../interfaces/activity/activity";
 import Activity from "../Activity/Activity";
 import "./ActivitiesGallery.css";
+import Paper from "@mui/material/Paper";
 
 interface props {
   selectedActivities: activity[];
 }
 export default function ActivitiesGallery({ selectedActivities }: props) {
   return (
-    <Stack spacing={{ xs: 1, sm: 1, md: 5 }}>
-      <div className="activities-list">
-        {selectedActivities?.map((activity: activity, index) => (
-          <Activity activity={activity} isSelected />
-        ))}
-      </div>
-    </Stack>
+    <Paper elevation={0} style={{ maxHeight: "25vh", overflow: "auto", backgroundColor:"transparent" }}>
+      <Stack spacing={{ xs: 1, sm: 1, md: 5 }}>
+        <div className="activities-list">
+          {selectedActivities?.map((activity: activity, index) => (
+            <Activity activity={activity} isSelected />
+          ))}
+        </div>
+      </Stack>
+    </Paper>
   );
 }
