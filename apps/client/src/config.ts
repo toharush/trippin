ecd6b5985e030a25fac24f0a64725bca183bc591
YaronZandberg <52877527+YaronZandberg@users.noreply.github.com>
2023-06-20 17:31:39 +0300

    fix

--- apps/client/src/config.ts
+++ apps/client/src/config.ts
@@ -1,5 +1,5 @@
-// export const APP_HOST = "193.106.55.139";
-export const APP_HOST = "localhost";
-export const APP_PORT = 8080;
+export const APP_HOST = "193.106.55.139";
+// export const APP_HOST = "localhost";
+export const APP_PORT = 81;
 export const MAX_RADIUS = 50;
 export const MAX_ACTIVITIES = 100000;
